27a80197f063f4b66147cc57c9926006baabf3db
Kamush999 <ilyapat99@gmail.com>
2023-04-05 12:06:33 +0600

    Настройка css modules

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import {Counter} from "./Counter";
+import "./index.scss"
+const App = () => {
+    return (
+        <div className="app">
+            sdasfkjfaf
+            <Counter/>
+        </div>
+    );
+};
+
+export default App;
