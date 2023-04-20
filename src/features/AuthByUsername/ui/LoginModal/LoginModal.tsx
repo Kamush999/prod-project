689e88b324a35284297bff6137e1d04668d7f2b5
Kamush999 <ilyapat99@gmail.com>
2023-04-20 10:56:11 +0600

    Fix errors

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,24 +1,17 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
-    className?: string;
     isOpen: boolean;
     onClose: () => void;
 }
 export const LoginModal = (props: LoginModalProps) => {
-    const { t } = useTranslation();
     const {
-        className,
         isOpen,
         onClose,
     } = props;
     return (
         <Modal
-            className={classNames(cls.LoginModal, {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
             lazy
